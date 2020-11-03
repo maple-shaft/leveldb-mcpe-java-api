37e84100b78ce5cdc396121fe77f9627a313255e
Dustin Briscoe <alain_us@hotmail.com>
2020-11-02 20:46:51 -0500

    Update AbstractInstruction.java

--- dasm/src/main/java/org/middlepath/dassembler/AbstractInstruction.java
+++ dasm/src/main/java/org/middlepath/dassembler/AbstractInstruction.java
@@ -7,14 +7,15 @@ public interface AbstractInstruction {
 	public static final byte ADD_NOOP = 0x03;
 	public static final byte LDA_REF = 0x04;
 	public static final byte LDB_REF = 0x05;
-	public static final byte STA_REF = 0x06;
-	public static final byte STB_REF = 0x07;
-	public static final byte JMP_IMM = 0x08;
-	public static final byte JMP_REF = 0x09;
-	public static final byte SUB_NOOP = 0x0A;
-	public static final byte BEQ_IMM = 0x0B;
+	public static final byte BEQ_IMM = 0x06;
+	public static final byte JMP_IMM = 0x07;
+	public static final byte STA_REF = 0x08;
+	public static final byte STB_REF = 0x09;
+	public static final byte OUT_REF = 0x0A;
+	public static final byte SUB_NOOP = 0x0B;
 	public static final byte BEQ_REF = 0x0C;
-	public static final byte OUT_REF = 0x0D;
+	public static final byte JMP_REF = 0x0D;
+	
 	
 	byte getValue();
 	
